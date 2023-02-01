68dddd5c78c1c8106795c9dd6af75de6eb131493
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-01-31 17:22:20 -0800

    fixed questions structure

---
+++ assests/script.js
@@ -0,0 +1,41 @@
+// Create an object with questions and answers
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+var package = {
+    question: "Who is the richest hero in all of comics?",
+    choices:[1,2,3,4],
+    answer:1,
+}
+// Create a function to start the game
+// Create a function to compare answers
+// Create a function to start a timer 
+// Create a function to calculate a score 
+// Create a function to save scores in local storage
