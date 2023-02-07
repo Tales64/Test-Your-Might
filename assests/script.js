02f01d2cfb5c7926e10dc219b4b143df6fe5a68a
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-02-06 17:58:31 -0800

    added variables and places for the options to be displayed

--- assests/script.js
+++ assests/script.js
@@ -1,3 +1,17 @@
+
+var submitEl = document.querySelector("#submit")
+var startButton = document.querySelector("#start")
+var timer = document.querySelector("#timeLeft")
+var scorePlace = document.querySelector("#scorePlace")
+
+var questionIndex = 0
+var score = 0
+var initials = document.querySelector("#initialsInput");
+
+var highScores = JSON.parse(localStorage.getItem('highscores')) || [];
+var ul = document.getElementById("highScores")
+
+
 // Create an object with questions and answers
 var package = [{
     question: "Who is the richest hero in all of comics?",
@@ -42,11 +56,23 @@ var package = [{
 
 function startGame() {
                       
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
+}
 
+function nextQuestion(){
+    console.log(question[questionIndex].corr)
+    
+    // the questions and answers in their own boxes
+    var currentQuest = document.querySelector("#question");
+    var answer1 = document.querySelector("#answer1");
+    var answer2 = document.querySelector("#answer2");
+    var answer3 = document.querySelector("#answer3");
+    var answer4 = document.querySelector("#answer4");
+    currentQuest.textContent = package[questionIndex].quest;
+    answer1.textContent = package[questionIndex].choices[0];
+    answer2.textContent = package[questionIndex].choices[1];
+    answer3.textContent = package[questionIndex].choices[2];
+    answer4.textContent = package[questionIndex].choices[3];
+    
 }
 
 
