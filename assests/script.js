3258866e653e00a978444e80b28af8320bd44d31
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-02-06 16:37:56 -0800

    changed html structure and fix my question object

--- assests/script.js
+++ assests/script.js
@@ -1,41 +1,60 @@
 // Create an object with questions and answers
-var package = {
+var package = [{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-var package = {
+},
+{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-var package = {
+},
+{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-var package = {
+},
+{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-var package = {
+},
+{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-var package = {
+},
+{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-var package = {
+},
+{
     question: "Who is the richest hero in all of comics?",
     choices:[1,2,3,4],
     answer:1,
-}
-// Create a function to start the game
-// Create a function to compare answers
-// Create a function to start a timer 
-// Create a function to calculate a score 
+}]
+
+// Create Variables for Local storage
 // Create a function to save scores in local storage
+
+// Create a function to start the game
+
+function startGame() {
+                      
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+
+}
+
+
+
+// Create a function to compare answers
+
+// Create a function to start a timer 
+
+// Create a function to calculate a score 
+
+
